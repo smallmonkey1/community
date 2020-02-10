64ce6e9871660f37f81110052bdd41152a8347f4
smallmonkey1 <1226408278@qq.com>
2020-02-10 19:46:40 +0800

    add index and bootstrap

--- src/main/java/com/z/community/controller/IndexController.java
+++ src/main/java/com/z/community/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.z.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * project--community.
@@ -11,8 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
 
-    @GetMapping("/index")
+    @RequestMapping("/")
     public String Index(){
         return "index";
     }
+
 }
